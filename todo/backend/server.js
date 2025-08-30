1c11be89ecf9362288a7c6149ed5bbb01d2d9c8e
ABU BAKR <osamudazai6781@gmail.com>
2025-08-30 21:51:16 +0530

    Update server.js

--- todo/backend/server.js
+++ todo/backend/server.js
@@ -12,7 +12,7 @@ const MONGO=process.env.MONGO_URI
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors({
   origin: "http://localhost:5173",
